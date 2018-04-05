71b94788b91b0da3a911431872eae7935ba9bb8f
chandan sony <chandansonysistec@gmail.com>
2018-04-06 03:02:27 +0530

    chandan first commit by chandan 2

---
+++ app/src/main/java/com/example/krishanasony/e_billing/Cong/Cong.java
@@ -0,0 +1,5 @@
+package com.example.krishanasony.e_billing.Cong;
+
+public class Cong {
+    public static final String PAYPAL_CLIENT_ID = "Adqor2UkAkwZXgmKkJ4sZiuPmZrBCdNf9Df13rVfN0wlOb_xpaeg-ZgYGzQMhjaXktHD2GdTPTCvLzpV";
+}
