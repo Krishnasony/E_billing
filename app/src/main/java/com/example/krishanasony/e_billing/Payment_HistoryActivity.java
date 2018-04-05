39419d74a57cc17a42d0fd4fc5d744e82705da4a
chandan sony <chandansonysistec@gmail.com>
2018-04-05 22:16:16 +0530

    chandan first commit

--- app/src/main/java/com/example/krishanasony/e_billing/Payment_HistoryActivity.java
+++ app/src/main/java/com/example/krishanasony/e_billing/Payment_HistoryActivity.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.Query;
 public class Payment_HistoryActivity extends AppCompatActivity {
     RecyclerView recyclerView=null;
     DatabaseReference mdatabase;
+    //// toh open kr or branch bna
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
